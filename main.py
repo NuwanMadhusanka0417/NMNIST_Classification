3c2b79fa5a37c56046fbfee9fbc88953e3852795
NuwanMadhusanka0417 <madhusanka0417@gmail.com>
2025-08-18 21:11:29 +1000

    WORKING

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     # GRAPH parameters
     DATA_NAME = "NCARS" # NCARS, NMNIST
     DATA_PATH = "data"
-    DATASET = "full"  # full / test      size of dataset loading for training and testing
+    DATASET = "test"  # full / test      size of dataset loading for training and testing
 
     if DATA_NAME == "NCARS":
         X_MAX = 360
@@ -28,7 +28,8 @@ def main():
         T_STEP = 10_000
 
     NORMALIZE_FEAT = False
-    NUM_OF_GRAPH_EVENTS = 100  # None, 10, 50, 100. etc
+    NUM_OF_GRAPH_EVENTS = None  # None, 10, 50, 100. etc
+    
     R = 7
     D_MAX = 30
 
@@ -39,7 +40,7 @@ def main():
     NR_MINIMUM_EVENTS = 3
 
     # GVFA parameters
-    HV_DIMENTION = 1000
+    HV_DIMENTION = 15000
     LAYERS = 5
     DELTA = 1  # 2
     EQUATION = 11
@@ -48,92 +49,72 @@ def main():
     # load event streams
     print("[LOG] - Loading events")
     # full_ev_ds = ev_loader(root=DATA_PATH, dataset=DATASET)
-    train_ds, test_ds = ev_loader(root=DATA_PATH, dataset=DATASET)
-
+    ds = ev_loader(root=DATA_PATH, dataset=DATASET)
+    train_ds, test_ds = train_test_split(ds, test_size=0.2, random_state=10, shuffle=True)  
     print("[LOG] - Making class objects.")
     MNISTGraph_model_train = NMNISTGraphDataset(tonic_raw_dataset=train_ds, num_of_graph_events=NUM_OF_GRAPH_EVENTS,
                                           R=R, Dmax=D_MAX,
                                           noise_remove=NOICE_REMOVED, normalized_feat=NORMALIZE_FEAT,
                                           nr_bin_xy_size=NR_BIN_XY_SIZE, nr_minimum_events=NR_MINIMUM_EVENTS,
                                           nr_time_bin_size=NR_TIME_BIN_SIZE)
-
+    
     MNISTGraph_model_test = NMNISTGraphDataset(tonic_raw_dataset=test_ds, num_of_graph_events=NUM_OF_GRAPH_EVENTS,
                                                 R=R, Dmax=D_MAX,
                                                 noise_remove=NOICE_REMOVED, normalized_feat=NORMALIZE_FEAT,
                                                 nr_bin_xy_size=NR_BIN_XY_SIZE, nr_minimum_events=NR_MINIMUM_EVENTS,
                                                 nr_time_bin_size=NR_TIME_BIN_SIZE)
 
-    gvfa_model = GraphCNN(input_dim=HV_DIMENTION, num_layers=LAYERS, delta=DELTA, graph_pooling_type="sum",
-                          neighbor_pooling_type="sum", device=DEVICE, equation=EQUATION).to(DEVICE)
-    cb = CodeBook(dim=HV_DIMENTION, x_max=X_MAX, y_max=Y_MAX, t_max=T_MAX, t_step=T_STEP)
-    hvs = HVs(codebook=cb, gvfa_model=gvfa_model)
+    items = [7000, 10000, 15000]
+    for item in items:
+        HV_DIMENTION = item
+        gvfa_model = GraphCNN(input_dim=HV_DIMENTION, num_layers=LAYERS, delta=DELTA, graph_pooling_type="sum",
+                            neighbor_pooling_type="sum", device=DEVICE, equation=EQUATION).to(DEVICE)
+        cb = CodeBook(dim=HV_DIMENTION, x_max=X_MAX, y_max=Y_MAX, t_max=T_MAX, t_step=T_STEP)
+        hvs = HVs(codebook=cb, gvfa_model=gvfa_model)
 
 
-    X_train, X_test, y_train, y_test = [],[],[],[]
-    print("[LOG] - Loading graph and converting to HVs.")
-    for i in range(len(train_ds)):
-        print(i)
-        g = MNISTGraph_model_train.get(i)
-        # print(g)
-        x, y = hvs.make_hvs(graph=g)
-        X_train.append(x)
-        y_train.append(y)
+        X_train, X_test, y_train, y_test = [],[],[],[]
+        print("[LOG] - Loading graph and converting to HVs.")
+        for i in range(len(ds)):
+            g = MNISTGraph_model_train.get(i)
+            x, y = hvs.make_hvs(graph=g)
+            X_train.append(x)
+            y_train.append(y)
+        
+        for i in range(len(test_ds)):
+            g = MNISTGraph_model_test.get(i)
+            x, y = hvs.make_hvs(graph=g)
+            X_test.append(x)
+            y_test.append(y)
 
-    for i in range(len(test_ds)):
-        print(i)
-        g = MNISTGraph_model_test.get(i)
-        # print(g)
-        x, y = hvs.make_hvs(graph=g)
-        X_test.append(x)
-        y_test.append(y)
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+        # X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42, shuffle=True)
+        del cb
+        del hvs
+        del gvfa_model
+        # del full_ev_ds
 
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
+        el = [0.1, 0.5, 1, 3]
+        for elm in el:
+            print(elm)
+            print("[LOG] - Classification.")
 
-    scaler1 = StandardScaler()
-    X_test = scaler1.fit_transform(X_test)
+            clf = SVC(kernel="rbf", C=elm,class_weight="balanced", gamma='scale')  # best in 100 - c = 3, weight = NOne, gamma = scale SVC(kernel="rbf", C=0.1, gamma=0.9,degree=6)
+    
+            clf.fit(X_train, y_train)
 
-    del cb
-    del hvs
-    del gvfa_model
-    # del full_ev_ds
+            print(f"Train accuracy: {accuracy_score(y_train, clf.predict(X_train)) * 100:.2f}%")
+            print(f"Test  accuracy: {accuracy_score(y_test, clf.predict(X_test)) * 100:.2f}%")
 
-    el = [1000]
-    for elm in el:
-        print(elm)
-        print("[LOG] - Classification.")
+            print("[LOG]- NUM_OF_GRAPH_EVENTS:", NUM_OF_GRAPH_EVENTS, " | DATASET:", DATASET,
+                " | NORMALIZE_FEAT:", NORMALIZE_FEAT,
+                " | R:", R, " | D_MAX: ", D_MAX, " | NOICE_REMOVED: ", NOICE_REMOVED,
+                " | NR_BIN_XY_SIZE: ", NR_BIN_XY_SIZE, " | NR_TIME_BIN_SIZE: ", NR_TIME_BIN_SIZE, " | NR_MINIMUM_EVENTS: ",
+                NR_MINIMUM_EVENTS, " | HV_DIMENTION: ", HV_DIMENTION," | LAYERS: ", LAYERS," | DELTA: ", DELTA," | EQUATION: ", EQUATION,)
 
-        # clf = SVC(kernel="rbf", C=0.1, gamma=0.9,degree=6)
-        pipe_rbf = Pipeline([
-            ("sc", StandardScaler(with_mean=False)),
-            ("svc", SVC(kernel="rbf"))
-        ])
-
-        param_grid = {
-            "svc__C": [0.1, 1, 3],
-            "svc__gamma": ["scale", 1e-3, 1e-2, 1e-1],
-            "svc__class_weight": [None, "balanced"]
-        }
-
-        grid = GridSearchCV(pipe_rbf,
-                            param_grid=param_grid,
-                            n_jobs=-1,
-                            verbose=1)
-        grid.fit(X_train, y_train)
-
-
-        print(f"Train accuracy: {accuracy_score(y_train, grid.predict(X_train)) * 100:.2f}%")
-        print(f"Test  accuracy: {accuracy_score(y_test, grid.predict(X_test)) * 100:.2f}%")
-
-        print("[LOG]- NUM_OF_GRAPH_EVENTS:", NUM_OF_GRAPH_EVENTS, " | DATASET:", DATASET,
-              " | NORMALIZE_FEAT:", NORMALIZE_FEAT,
-              " | R:", R, " | D_MAX: ", D_MAX, " | NOICE_REMOVED: ", NOICE_REMOVED,
-              " | NR_BIN_XY_SIZE: ", NR_BIN_XY_SIZE, " | NR_TIME_BIN_SIZE: ", NR_TIME_BIN_SIZE, " | NR_MINIMUM_EVENTS: ",
-              NR_MINIMUM_EVENTS, " | HV_DIMENTION: ", HV_DIMENTION," | LAYERS: ", LAYERS," | DELTA: ", DELTA," | EQUATION: ", EQUATION,)
-        print(grid.best_params_)
-        print(grid.best_score_)
-        print(grid.param_grid)
-        # del clf
+            del clf
 
 if __name__ == "__main__":
     main()
---
+++ main_100.py
@@ -0,0 +1,169 @@
+from src.graph_to_vec_converter import  HVs
+from sklearn.metrics       import accuracy_score, classification_report
+from sklearn.linear_model  import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from src.graph_generation import NMNISTGraphDataset
+from src.loader import ev_loader
+from src.graphcnnVSA_Binding_FULL import GraphCNN
+from src.codebook import CodeBook
+import torch
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing   import StandardScaler
+from sklearn.svm             import SVC
+from sklearn.pipeline        import Pipeline
+from sklearn.metrics         import accuracy_score
+from torch.utils.data import ConcatDataset
+
+
+print("[LOG] - parameter initialization.")
+# GRAPH parameters
+DATA_NAME = "NCARS" # NCARS, NMNIST
+DATA_PATH = "data"
+DATASET = "full"  # full / test      size of dataset loading for training and testing
+
+if DATA_NAME == "NCARS":
+    X_MAX = 360
+    Y_MAX = 360
+    T_MAX = 100_000_000
+    T_STEP = 10_000
+
+NORMALIZE_FEAT = False
+NUM_OF_GRAPH_EVENTS = 100  # None, 10, 50, 100. etc
+R = 7
+D_MAX = 30
+
+# NOISE parameters
+NOICE_REMOVED = True
+NR_BIN_XY_SIZE = 4
+NR_TIME_BIN_SIZE = 20_000
+NR_MINIMUM_EVENTS = 3
+
+# GVFA parameters
+HV_DIMENTION = 1000
+LAYERS = 5
+DELTA = 1  # 2
+EQUATION = 11
+DEVICE = torch.device("cpu")
+
+# load event streams
+print("[LOG] - Loading events")
+# full_ev_ds = ev_loader(root=DATA_PATH, dataset=DATASET)
+ds = ev_loader(root=DATA_PATH, dataset=DATASET)
+
+train_ds, test_ds = train_test_split(ds, test_size=0.2, random_state=10, shuffle=True)   
+print("[LOG] - Making class objects.")
+MNISTGraph_model_train = NMNISTGraphDataset(tonic_raw_dataset=ds, num_of_graph_events=NUM_OF_GRAPH_EVENTS,
+                                      R=R, Dmax=D_MAX,
+                                      noise_remove=NOICE_REMOVED, normalized_feat=NORMALIZE_FEAT,
+                                      nr_bin_xy_size=NR_BIN_XY_SIZE, nr_minimum_events=NR_MINIMUM_EVENTS,
+                                      nr_time_bin_size=NR_TIME_BIN_SIZE)
+
+
+
+MNISTGraph_model_test_50 = NMNISTGraphDataset(tonic_raw_dataset=test_ds, num_of_graph_events=50,
+                                            R=R, Dmax=D_MAX,
+                                            noise_remove=NOICE_REMOVED, normalized_feat=NORMALIZE_FEAT,
+                                            nr_bin_xy_size=NR_BIN_XY_SIZE, nr_minimum_events=NR_MINIMUM_EVENTS,
+                                            nr_time_bin_size=NR_TIME_BIN_SIZE)
+
+MNISTGraph_model_test_10 = NMNISTGraphDataset(tonic_raw_dataset=test_ds, num_of_graph_events=10,
+                                            R=R, Dmax=D_MAX,
+                                            noise_remove=NOICE_REMOVED, normalized_feat=NORMALIZE_FEAT,
+                                            nr_bin_xy_size=NR_BIN_XY_SIZE, nr_minimum_events=NR_MINIMUM_EVENTS,
+                                            nr_time_bin_size=NR_TIME_BIN_SIZE)
+
+items = [1000, 5000, 7000]
+for item in items:
+    HV_DIMENTION = item
+    gvfa_model = GraphCNN(input_dim=HV_DIMENTION, num_layers=LAYERS, delta=DELTA, graph_pooling_type="sum",
+                          neighbor_pooling_type="sum", device=DEVICE, equation=EQUATION).to(DEVICE)
+    cb = CodeBook(dim=HV_DIMENTION, x_max=X_MAX, y_max=Y_MAX, t_max=T_MAX, t_step=T_STEP)
+    hvs = HVs(codebook=cb, gvfa_model=gvfa_model)
+
+
+    X_,X_test_50_,X_test_10_, Y_, y_test_50_10 = [],[],[],[], []
+    print("[LOG] - Loading graph and converting to HVs.")
+    for i in range(len(ds)):
+        # print(i)
+        g = MNISTGraph_model_train.get(i)
+        x, y = hvs.make_hvs(graph=g)
+        X_.append(x)
+        Y_.append(y)
+
+
+    X_train_, X_test_, y_train, y_test = train_test_split(X_, Y_, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train_)
+    X_test = scaler.transform(X_test_)
+
+    for i in range(len(test_ds)):
+        # print(i)
+        g_50 = MNISTGraph_model_test_50.get(i)
+        g_10 = MNISTGraph_model_test_10.get(i)
+
+        # print(g)
+
+        x_50, y = hvs.make_hvs(graph=g_50)
+        x_10, _ = hvs.make_hvs(graph=g_10)
+
+
+        X_test_50_.append(x_50)
+        X_test_10_.append(x_10)
+        y_test_50_10.append(y)
+
+
+    X_test_50 = scaler.fit_transform(X_test_50_)
+    X_test_10 = scaler.fit_transform(X_test_10_)
+
+
+    del cb
+    del hvs
+    del gvfa_model
+    # del full_ev_ds
+
+    # el = [1000, 5000]
+
+    print("[LOG] - Classification.")
+
+    # clf = SVC(kernel="rbf", C=0.1, gamma=0.9,degree=6)
+    grid = SVC(kernel="rbf", C=3, gamma='scale', class_weight="balanced")
+    # pipe_rbf = Pipeline([
+    #     ("sc", StandardScaler(with_mean=False)),
+    #     ("svc", SVC(kernel="rbf"))
+    # ])
+
+    # param_grid = {
+    #     "svc__C": [0.1, 1, 3],
+    #     "svc__gamma": ["scale", 1e-3, 1e-2, 1e-1],
+    #     "svc__class_weight": [None, "balanced"]
+    # }
+
+    # grid = GridSearchCV(pipe_rbf,
+    #                     param_grid=param_grid,
+    #                     n_jobs=-1,
+    #                     verbose=1)
+    grid.fit(X_train, y_train)
+
+    # print(grid.best_params_)
+    # print(grid.best_score_)
+    # print(grid.param_grid)
+
+    print("----100------")
+    print(f"Train accuracy: {accuracy_score(y_train, grid.predict(X_train)) * 100:.2f}%")
+    print(f"Test  accuracy: {accuracy_score(y_test, grid.predict(X_test)) * 100:.2f}%")
+
+    print("----50------")
+    print(f"Test  accuracy: {accuracy_score(y_test_50_10, grid.predict(X_test_50)) * 100:.2f}%")
+
+    print("----10------")
+    print(f"Test  accuracy: {accuracy_score(y_test_50_10, grid.predict(X_test_10)) * 100:.2f}%")
+
+    print("[LOG]- NUM_OF_GRAPH_EVENTS:", NUM_OF_GRAPH_EVENTS, " | DATASET:", DATASET,
+          " | NORMALIZE_FEAT:", NORMALIZE_FEAT,
+          " | R:", R, " | D_MAX: ", D_MAX, " | NOICE_REMOVED: ", NOICE_REMOVED,
+          " | NR_BIN_XY_SIZE: ", NR_BIN_XY_SIZE, " | NR_TIME_BIN_SIZE: ", NR_TIME_BIN_SIZE, " | NR_MINIMUM_EVENTS: ",
+          NR_MINIMUM_EVENTS, " | HV_DIMENTION: ", HV_DIMENTION," | LAYERS: ", LAYERS," | DELTA: ", DELTA," | EQUATION: ", EQUATION,)
+
+        # del clf
