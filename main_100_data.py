cf94887e75d3b2b0147be737b31d96a5b6efae3b
NuwanMadhusanka0417 <madhusanka0417@gmail.com>
2025-08-19 23:30:25 +1000

    WORKING give idle results

--- main_100_data.py
+++ main_100_data.py
@@ -19,7 +19,7 @@ def main(normalized_feat, num_of_graph_events):
     print("[LOG] - parameter initialization.")
     # GRAPH parameters
     DATA_PATH = "data"
-    DATASET = "test"  # full / test      size of dataset loading for training and testing
+    DATASET = "full"  # full / test      size of dataset loading for training and testing
 
     NORMALIZE_FEAT = False
     NUM_OF_GRAPH_EVENTS = 100  # None, 10, 50, 100. etc
@@ -46,20 +46,20 @@ def main(normalized_feat, num_of_graph_events):
 
     ###################################################################
 
-    indices = np.arange(len(full_ev_ds))
-    labels  = [full_ev_ds[i][1] for i in indices]
-    train_idx, test_idx = train_test_split(
-        indices,
-        test_size=0.2,
-        random_state=42,
-        shuffle=True,
-        stratify=labels
-    )
+    # indices = np.arange(len(full_ev_ds))
+    # labels  = [full_ev_ds[i][1] for i in indices]
+    # train_idx, test_idx = train_test_split(
+    #     indices,
+    #     test_size=0.2,
+    #     random_state=42,
+    #     shuffle=True,
+    #     stratify=labels
+    # )
 
-    ds_train = Subset(full_ev_ds, train_idx.tolist())
-    ds_test  = Subset(full_ev_ds, test_idx.tolist())
+    # ds_train = Subset(full_ev_ds, train_idx.tolist())
+    # ds_test  = Subset(full_ev_ds, test_idx.tolist())
 
-    # ds_train, ds_test = train_test_split(full_ev_ds, test_size=0.2, random_state=42, shuffle=True)   
+    ds_train, ds_test = train_test_split(full_ev_ds, test_size=0.2, random_state=10, shuffle=True)   
     ######################################################################
     print("[LOG] - Making class objects.")
 
@@ -87,7 +87,7 @@ def main(normalized_feat, num_of_graph_events):
                                                   nr_bin_xy_size=NR_BIN_XY_SIZE, nr_minimum_events=NR_MINIMUM_EVENTS,
                                                   nr_time_bin_size=NR_TIME_BIN_SIZE)
     
-    HV_Dimensions = [10000]
+    HV_Dimensions = [15000]
     print("Start For loop")
     for HV_DIMENTION in HV_Dimensions:
 
@@ -126,65 +126,46 @@ def main(normalized_feat, num_of_graph_events):
             y_test_50.append(y_50)
             y_test_10.append(y_10)
         print("Start Classification")
-        CS = [0.5, 1]
+        CS = [0.01, 1, 3 ]
+        iterations = [120, 200, 500, 800]
         for c in CS:
-            print("C = ", c)
-            clf = LogisticRegression(
-                C=c,
-                solver='saga',       # handles high-dimensional sparse data
-                penalty='l2',        # ridge regularization
-                n_jobs=-1,           # parallelize over cores
-                max_iter=200,
-                random_state=42
-            )
+            for iters in iterations:
+                print("C = ", c)
+                clf = LogisticRegression(
+                    C=c,
+                    solver='saga',       # handles high-dimensional sparse data
+                    penalty='l2',        # ridge regularization
+                    n_jobs=-1,           # parallelize over cores
+                    max_iter=iters,
+                    random_state=42
+                )
 
-            # 2. Pipeline (with scaling for sparse/hypervector inputs)
-            # pipe = Pipeline([
-            #     ("scaler", StandardScaler(with_mean=False)),
-            #     ("lr",    clf)
-            # ])
+                scaler = StandardScaler()
+                X_train_100 = scaler.fit_transform(X_train_100)
+                X_test_100 = scaler.transform(X_test_100)
+                X_test_50 = scaler.fit_transform(X_test_50)
+                X_test_10 = scaler.fit_transform(X_test_50)
 
-            # # 3. Expanded hyperparameter grid (now including max_iter)
-            # param_grid = {
-            #     "lr__C":            [ 0.1, 1],  # 0.1
-            #     "lr__tol":          [ 1e-3, 1e-2],
-            #     "lr__class_weight": [None],
-            #     "lr__max_iter":     [1000, 2000]
-            # }
+                clf.fit(X_train_100, Y_train_100)
 
-            # # 4. GridSearchCV setup
-            # grid = GridSearchCV(
-            #     estimator=pipe,
-            #     param_grid=param_grid,
-            #     cv=5,                 # 5-fold CV
-            #     scoring="accuracy",
-            #     n_jobs=-1,
-            #     verbose=1
-            # )
+                print(f"----100------ C = {c}  iterations = {iters}   Dimention = {HV_DIMENTION}")
+                print(f"Train accuracy: {accuracy_score(Y_train_100, clf.predict(X_train_100)) * 100:.2f}%")
+                print(f"Test  accuracy: {accuracy_score(Y_test_100, clf.predict(X_test_100)) * 100:.2f}%")
 
-            clf.fit(X_train_100, Y_train_100)
-            # print(grid.best_params_)
-            # print(grid.best_score_)
-            # print(grid.param_grid)
-            
+                print("----50------")
+                print(f"Test  accuracy: {accuracy_score(y_test_50, clf.predict(X_test_50)) * 100:.2f}%")
 
-            print("----100------")
-            print(f"Train accuracy: {accuracy_score(Y_train_100, clf.predict(X_train_100)) * 100:.2f}%")
-            print(f"Test  accuracy: {accuracy_score(Y_test_100, clf.predict(X_test_100)) * 100:.2f}%")
+                print("----10------")
+                print(f"Test  accuracy: {accuracy_score(y_test_10, clf.predict(X_test_10)) * 100:.2f}%")
 
-            print("----50------")
-            print(f"Test  accuracy: {accuracy_score(y_test_50, clf.predict(X_test_50)) * 100:.2f}%")
-
-            print("----10------")
-            print(f"Test  accuracy: {accuracy_score(y_test_10, clf.predict(X_test_10)) * 100:.2f}%")
-
-            print("[LOG]- NUM_OF_GRAPH_EVENTS:", NUM_OF_GRAPH_EVENTS, " | DATASET:", DATASET,
-                " | NORMALIZE_FEAT:", NORMALIZE_FEAT,
-                " | R:", R, " | D_MAX: ", D_MAX, " | NOICE_REMOVED: ", NOICE_REMOVED,
-                " | NR_BIN_XY_SIZE: ", NR_BIN_XY_SIZE, " | NR_TIME_BIN_SIZE: ", NR_TIME_BIN_SIZE,
-                " | NR_MINIMUM_EVENTS: ",
-                NR_MINIMUM_EVENTS, " | HV_DIMENTION: ", HV_DIMENTION, " | LAYERS: ", LAYERS, " | DELTA: ", DELTA,
-                " | EQUATION: ", EQUATION, )
+                # print("[LOG]- NUM_OF_GRAPH_EVENTS:", NUM_OF_GRAPH_EVENTS, " | DATASET:", DATASET,
+                #     " | NORMALIZE_FEAT:", NORMALIZE_FEAT,
+                #     " | R:", R, " | D_MAX: ", D_MAX, " | NOICE_REMOVED: ", NOICE_REMOVED,
+                #     " | NR_BIN_XY_SIZE: ", NR_BIN_XY_SIZE, " | NR_TIME_BIN_SIZE: ", NR_TIME_BIN_SIZE,
+                #     " | NR_MINIMUM_EVENTS: ",
+                #     NR_MINIMUM_EVENTS, " | HV_DIMENTION: ", HV_DIMENTION, " | LAYERS: ", LAYERS, " | DELTA: ", DELTA,
+                #     " | EQUATION: ", EQUATION, )
+                del clf
         
         del gvfa_model
         del cb
@@ -197,7 +178,7 @@ def main(normalized_feat, num_of_graph_events):
         del Y_test_100
         del y_test_50
         del y_test_10
-        del clf
+        
 
 
 if __name__ == "__main__":
